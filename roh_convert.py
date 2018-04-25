abe4e22c5bad6c484242f9ba6489d7eadd82bfc4
Filip Dominec <filip.dominec@gmail.com>
2018-04-25 17:23:31 +0200

    accepts also pixel smoothing when dividing by calibration curve + printouts

--- roh_convert.py
+++ roh_convert.py
@@ -43,7 +43,6 @@ secondorderampli = 178./12900 + 322./4348 - 300./3176  #- 217./6307     # subtra
 
 def divide_spectrum():
     """ Measured for the calibration lamp """
-    #print("LOADING" ,os.path.join(os.path.dirname(os.path.realpath(__file__)), 'calibration_curve.dat'))
     return np.loadtxt(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'calibration_curve.dat'), unpack=True)[1] 
 def multiply_spectrum(xx):
     ## Provided by the calibration lamp manufacturer
@@ -66,13 +65,14 @@ keepsec = ('--keepsec' in   [ff for ff in sys.argv[1:] if ff[0:1] == '-'])
 
 for filepath in [ff for ff in sys.argv[1:] if ff[0:1] != '-']:
     my_roh = roh.Roh.from_file(filepath);
+    print('processing: ',  filepath)
 
     ## Avaspec generates the x-axis exactly this weird way
     x0 = np.arange(int(my_roh.ipixfirst)+2, int(my_roh.ipixlast)+1)  
     x  = my_roh.wlintercept + x0*my_roh.wlx1 + x0**2 *my_roh.wlx2 + x0**3 *my_roh.wlx3 + x0**4 *my_roh.wlx4
-    print('my_roh.integration_ms ', my_roh.integration_ms )
-    print('my_roh.averaging      ', my_roh.averaging      )
-    print('my_roh.pixel_smoothing', my_roh.pixel_smoothing)
+    print('    integration_ms  = ', my_roh.integration_ms )
+    print('    averaging       = ', my_roh.averaging      )
+    print('    pixel_smoothing = ', my_roh.pixel_smoothing)
 
       
       
@@ -81,11 +81,19 @@ for filepath in [ff for ff in sys.argv[1:] if ff[0:1] != '-']:
     spec = np.array(my_roh.spectrum)                          ## load the y-axis of the spectrum
 
     if not israw:
-        spec /= divide_spectrum()                             ## divide by the grating+CCD response
+        kernel = [1]*(1+int(my_roh.pixel_smoothing)*2)     # averaging of neighboring pixels is needed also for the calibration curve
+        kernel /= np.sum(kernel)                        # normalize the kernel
+        print('    applying calibration curve, smoothed by kernel', kernel)
+        smooth_divide = np.convolve(divide_spectrum(), kernel, mode='same')    # find the average value of neighbors
+        spec /= smooth_divide                             ## divide by the grating+CCD response
+
         spec /= my_roh.integration_ms                             ## divide by the integration time
         #spec *= multiply_spectrum(x)                   ## normalize to the spectral lamp XX included in calibration curve, 
+
+
                                                         ##   see also https://gist.github.com/FilipDominec/2aa3af9f558483a25b04628e60bdf7e7
     if not keepsec:
+        print('    subtracting second order with ratio ', secondorderampli)
         spec -= np.interp(x, x*2, spec*secondorderampli)      ## subtract the second-order grating artifact
 
     if not keepoutliers and not israw:
@@ -97,7 +105,7 @@ for filepath in [ff for ff in sys.argv[1:] if ff[0:1] != '-']:
         smooth2 = np.convolve(spec, kernel2, mode='same')    # find the average value of neighbors
         rms_noise = np.average((spec[1:]-spec[:-1])**2)**.5   # estimate what the average noise is (rms derivative)
         where_excess =  (np.abs(spec-smooth) > rms_noise*1)    # find all points with difference from average more than 3sigma
-        #print("Removing {} outliers".format(len(where_excess)))
+        print("    removing {} outliers".format(len(where_excess)))
         spec[where_excess] = smooth2[where_excess]
         #where_not_excess =  (np.abs(spec-smooth) < rms_noise*3)    # find all points with difference from average less than 3sigma
         #x, spec = x[where_not_excess], spec[where_not_excess]   # filter the data - WRONG, leads to uneven length of spectra
